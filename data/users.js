9e76630d033e661faa7c41972061cce911db1d3e
Salia Eunice <101419339+saliaeunice@users.noreply.github.com>
2022-04-07 17:07:14 +0100

    users API

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const users=[
+    {id:1, name:"James", age:18},
+    {id:2, name:"Jennifer", age:20},
+    {id:3, name:"Muniba", age:29},
+    {id:4, name:"Umietu", age:30},
+    {id:5, name:"Maxima", age:19}
+]
+module.exports=users
---
+++ index.js
@@ -0,0 +1,22 @@
+const express =require("express")
+const userRoute = require("./routes/usersRoutes")
+
+const app = express()
+
+//middlewires
+app.use(express.json());
+
+app.use(userRoute)
+
+//home route
+app.get("/", (req, res)=>{
+    res.status(200).send("<h1>WELCOME TO MY USERS DATABASE</h1>");
+})
+
+const PORT = 4000;
+
+
+app.listen(PORT, ()=>{
+console.log("SERVER IS UP")
+
+})
